81d570847299812433f16bfd543ad8834802e377
Ale Farias <mail.alefarias@gmail.com>
2019-08-29 22:09:46 -0300

    Adds polygon function and adds comments to other functions

--- primeiros_passos/first_steps.py
+++ primeiros_passos/first_steps.py
@@ -1,26 +1,41 @@
-from turtle import forward, right
+from turtle import forward, right, speed
 
 
-def square(side_size: int = 200):
+def square(side_length: int = 200):
+    """Make the turtle draws a square"""
     for i in range(4):
-        forward(side_size)
+        forward(side_length)
         right(90)
 
 
-def triangle(side_size: int = 300):
+def triangle(side_length: int = 300):
+    """Make the turtle draws a triangle"""
     for i in range(3):
-        forward(side_size)
+        forward(side_length)
         right(120)
 
 
 def multiples_geoms(geom_function, n: int, turn_angle: int = 5):
+    """Make the turtle draw a specific geom multiple types
+    turning right a specific angle in each time the geom is
+    draw
+    """
     for i in range(n):
         geom_function(200)
         right(turn_angle)
 
 
+def polygon(n_sides: int, side_length: int = 100):
+    """Draw a polygon of equilateral sides of length side_length"""
+    if n_sides < 3:
+        raise ValueError("The number of sides must be at least 3")
+    external_angle = 360/n_sides
+    for i in range(n_sides):
+        forward(side_length)
+        right(external_angle)
+
+
 if __name__ == "__main__":
-    turn_angle = 5
-    n_geoms = int(360/turn_angle)
-    multiples_geoms(square, n_geoms, turn_angle)
+    speed(1)
+    polygon(1000, 1)
     input('enter to finish...')
